4640bd3f2125a293d06665d1eb758891e71c3564
Yash <yashanchule909@gmail.com>
2026-09-25 19:10:45 +0530

    Added the session builing Session maker and testd it using an endpoint

--- app/db/database.py
+++ app/db/database.py
@@ -1,9 +1,23 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
 
 DATABASE_URL = "postgresql+psycopg://postgres:postgres@localhost:5432/personal_ai"
 
 engine = create_engine(DATABASE_URL)
 
+SessionLocal = sessionmaker(bind=engine)
+
+def get_db():
+
+    db = SessionLocal() 
+    # se we r just making sesion using the connection of the engine we had earlier and not just insitlasing 
+    # but also we will be closing this respectively !!This might help us a lot 
+    try:
+        yield db
+    finally:
+        db.close()
 # postgresql+psycopg://postgres:postgres@localhost:5432/personal_ai
 #        │        │       │        │        │
 #        │        │       │        │        └── database
@@ -11,4 +25,3 @@ engine = create_engine(DATABASE_URL)
 #        │        │       └────────────────── host
 #        │        └────────────────────────── password
 #        └────────────────────────────────── user/driver
-
--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Depends
 from sqlalchemy import text
-from db.database import engine
+from app.db.database import engine,get_db
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message" : "Hello World"}
+
+
+@app.get("/session_test")
+def session_test(db = Depends(get_db)):
+    result = db.execute(text("SELECT 1"))
+    value = result.scalar()
+    return {"database" :value}
 
 
 @app.get("/db-test")
